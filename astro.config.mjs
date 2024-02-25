eb2b6fa84134befb2750327d84846e64c3992f5f
Dan Dumont <ddumont@linksquares.com>
2024-02-25 14:07:45 -0500

    clean out react modules

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 
 /* 
   We are doing some URL mumbo jumbo here to tell Astro what the URL of your website will be.
@@ -28,5 +29,5 @@ if (isBuild) {
 export default defineConfig({
   server: { port: SERVER_PORT },
   site: 'https://ddumont.dev',
-  integrations: [sitemap()],
+  integrations: [react(), sitemap()],
 });
